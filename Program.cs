24e8fde46bce7d42a6cd440e16043f92f5edd203
Jambuzu <121358202+Jambuzu@users.noreply.github.com>
2023-02-01 22:31:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter an operator (+, -, *, /):");
+            char op = char.Parse(Console.ReadLine());
+
+            double result;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    return;
+            }
+
+            Console.WriteLine("Result: {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
